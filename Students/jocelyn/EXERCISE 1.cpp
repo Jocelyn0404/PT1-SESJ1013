fec4aacfe71e90e78aee7449208aba09ec2224cc
Jocelynwyx <128278945+Jocelyn0404@users.noreply.github.com>
2023-10-22 12:55:17 +0800

    Add files via upload

---
+++ Students/jocelyn/EXERCISE 1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+int integer;
+float num;
+char character;
+string array;
+
+	cout << "Enter an integer : ";
+    cin >> integer;
+
+    cout << "Enter a decimal number : ";
+    cin >> num;
+
+    cout << "Enter a single character : ";
+    cin >> character;
+
+    cout << "Enter an array of characters : ";
+    cin >> array;
+	
+	return 0;
+}
