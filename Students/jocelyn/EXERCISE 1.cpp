9552382f4a4fdf538d2cde280008df7e21a53b77
Jocelynwyx <128278945+Jocelyn0404@users.noreply.github.com>
2023-10-26 09:27:23 +0800

    Update EXERCISE 1.cpp

--- Students/jocelyn/EXERCISE 1.cpp
+++ Students/jocelyn/EXERCISE 1.cpp
@@ -1,24 +1,25 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main(){
-	
-int integer;
-float num;
-char character;
-string array;
+int main() {
+    int integer;
+    float num;
+    char character;
+    char array[100];
 
-	cout << "Enter an integer : ";
+    cout << "Enter an integer: ";
     cin >> integer;
 
-    cout << "Enter a decimal number : ";
+    cout << "Enter a decimal number: ";
     cin >> num;
 
-    cout << "Enter a single character : ";
+    cout << "Enter a single character: ";
     cin >> character;
 
-    cout << "Enter an array of characters : ";
-    cin >> array;
-	
-	return 0;
+    cout << "Enter an array of characters: ";
+    cin.ignore();
+    cin.getline(array, 100);
+
+    return 0;
 }
